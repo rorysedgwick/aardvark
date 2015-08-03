0a482bbc2b2270430ebbe26a5c0deab2937c6efb
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-08-03 23:08:53 +0100

    continue of spike, browserify experimentation with test files

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var data =  [
+var data = [
   {
     "country": "afghanistan",
     "code": "af"
@@ -997,35 +997,9 @@ var data =  [
     "code": "zw"
   }
 ]
+ module.exports = data;
 
 },{}],2:[function(require,module,exports){
 var data = require("./data.js");
 
-var selected = Math.floor(Math.random() * data.length);
-
-document.getElementById("current-country").innerText = data[selected].code;
-
-function matchChecker() {
-
-  if (document.getElementById("submission-box").value === data[selected].country) {
-    console.log("match");
-  }
-}
-
-var runner = require("../../test/runner.js");
-
-},{"../../test/runner.js":4,"./data.js":1}],3:[function(require,module,exports){
-test("The dataset should be stored in an array", function() {
-  equal(Object.prototype.toString.call(data), "[object Array]", "the country data is stored in an array");
-});
-
-test("The country-code div displays the text from an element in the countryCodes array", function() {
-  var currentCountry = document.getElementById("current-country");
-
-  equal(currentCountry.innerText, data[selected].code, "a country code is displayed in the div");
-});
-
-},{}],4:[function(require,module,exports){
-require("./front/_index.js");
-
-},{"./front/_index.js":3}]},{},[2]);
+},{"./data.js":1}]},{},[2]);
